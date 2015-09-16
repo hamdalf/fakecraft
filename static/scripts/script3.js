897f4376e433018b5a5fd2f92b2d9a612a1b86bc
Kyoung Woo Ham <kyoungwoo.ham@gmail.com>
2015-09-16 12:05:09 +0200

    make background

--- static/scripts/script3.js
+++ static/scripts/script3.js
@@ -1,13 +1,16 @@
 require(
     [
         '/scripts/threex/threex.windowresize.js',
+        '/scripts/threejs/tween.min.js',
+        '/scripts/threejs/flow.js',
         '/scripts/threex/threex.animation.js',
         '/scripts/threex/threex.animations.js',
         '/scripts/threex/threex.minecraft.js',
         '/scripts/threex/threex.minecraftcharheadanim.js',
         '/scripts/threex/threex.minecraftcharbodyanim.js',
         '/scripts/threex/threex.minecraftcontrols.js',
-        '/scripts/threex/threex.minecraftplayer.js'
+        '/scripts/threex/threex.minecraftplayer.js',
+        '/scripts/threex.volumetricspotlight/package.require.js'
     ], function () {
         if (!Detector.webgl) {
             Detector.addGetWebGLMessage();
@@ -21,6 +24,9 @@ require(
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.01, 1000);
         camera.position.z = 3;
+        camera.position.x = 0;
+        camera.position.y = 3;
+        camera.lookAt(new THREE.Vector3(0,0,-2));
         
         var onRenderFcts = [];
         
@@ -54,10 +60,98 @@ require(
                     character.root.rotation.y = Math.random()*Math.PI*2;
                     
                     character.loadWellKnownSkinRandomly();
-
+                    
+                    var headAnims = new THREEx.MinecraftCharHeadAnimations(character);
+                    //this.headAnims = headAnims;
+                    onRenderFcts.push(function(delta, now) {
+                        headAnims.update(delta, now);
+                    });
+                    var randAction = Math.round(Math.random() * 2);
+                    switch (randAction) {
+                        case 0:
+                            headAnims.start('yes');
+                            break;
+                        case 1:
+                            headAnims.start('no');
+                            break;
+                        case 2:
+                            headAnims.start('still');
+                            break;
+                    }
+                    
+                    var bodyAnims = new THREEx.MinecraftCharBodyAnimations(character);
+                    //this.bodyAnims = bodyAnims;
+                    onRenderFcts.push(function(delta, now) {
+                        bodyAnims.update(delta, now);
+                    });
+                    
+                    randAction = Math.round(Math.random() * 2);
+                    switch (randAction) {
+                        case 0:
+                            bodyAnims.start('strafe');
+                            break;
+                        case 1:
+                            bodyAnims.start('wave');
+                            break;
+                        case 2:
+                            bodyAnims.start('hiwave');
+                            break;
+                    }
                 })();
             }
         }
+        
+        THREEx.MinecraftChar.defaultMaterial = THREE.MeshPhongMaterial;
+        
+        var player1 = new THREEx.MinecraftPlayer();
+        player1.setNickname('Hamdalf');
+        scene.add(player1.character.root);
+        player1.character.root.traverse(function(object3d) {
+            object3d.receiveShadow = true;
+            object3d.castShadow = true;
+        });
+        onRenderFcts.push(function(delta, now) {
+            player1.update(delta, now);
+        });
+        
+        var player2 = new THREEx.MinecraftPlayer();
+        player2.setNickname('Batman');
+        player2.character.loadWellKnownSkin('batman');
+        player2.character.root.position.x = -1;
+        scene.add(player2.character.root);
+        player2.character.root.traverse(function(object3d) {
+            object3d.receiveShadow = true;
+            object3d.castShadow = true;
+        });
+        onRenderFcts.push(function(delta, now) {
+            player2.update(delta, now);
+        });
+        
+        onRenderFcts.push(function() {
+            TWEEN.update();
+        });
+        
+        // back wall
+        var geometry = new THREE.BoxGeometry(40, 0.1, 20, 20, 1, 20);
+        var material = new THREE.MeshPhongMaterial({
+            color: new THREE.Color('white')
+        });
+        var mesh = new THREE.Mesh(geometry, material);
+        mesh.receiveShadow = true;
+        mesh.castShadow = true;
+        mesh.rotateX(Math.PI/2);
+        mesh.position.set(0, -geometry.parameters.height/2, -4);
+        scene.add(mesh);
+        
+        // spot light
+        (function() {
+           var geometry = new THREE.CylinderGeometry(0.1, 1.5, 5, 32*2, 20, true); 
+           geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, -geometry.parameters.height/2, 0));
+           geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+           
+           var material = new THREEx.VolumetricSpotLightMaterial();
+           
+        });
         
         onRenderFcts.push(function() {
             renderer.render(scene, camera);
---
+++ static/scripts/threex.volumetricspotlight/package.require.js
@@ -0,0 +1,5 @@
+define( [ './threex.volumetricspotlightmaterial.js',
+		'./threex.volumetricspotlightmaterialdatgui.js',
+		'./threex.volumetricspotlightmesh.js'
+	], function(){
+});
