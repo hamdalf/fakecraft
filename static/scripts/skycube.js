877795fbbc3f7d767e58d5bfd6a6c5ad6f791da5
Kyoung Woo Ham <kyoungwoo.ham@gmail.com>
2015-10-07 16:52:29 +0200

    Study more

---
+++ static/scripts/skycube.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var urlPrefix = '/images/skycube/',
+		urls = [
+			urlPrefix + 'posx.jpg',
+			urlPrefix + 'negx.jpg',
+			urlPrefix + 'posy.jpg',
+			urlPrefix + 'posx.jpg',
+			urlPrefix + 'posx.jpg',
+			urlPrefix + 'posx.jpg'
+		];
+});
