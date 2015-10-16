5aabb533402636c8021849176e1913a74a604e6c
Kyoung Woo Ham <kyoungwoo.ham@gmail.com>
2015-10-16 17:43:55 +0200

    voxel is moved

--- static/scripts/mapeditor.js
+++ static/scripts/mapeditor.js
@@ -4,17 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 	
 	var cubeColors = [
-		new THREE.Color().setRGB(1, 0, 0),
-		new THREE.Color().setRGB(0, 1, 0),
-		new THREE.Color().setRGB(0, 0, 1),
-		new THREE.Color().setRGB(1, 1, 0),
-		new THREE.Color().setRGB(1, 0, 1),
-		new THREE.Color().setRGB(0, 1, 1),
-		new THREE.Color().setRGB(1, 0.5, 0.5),
-		new THREE.Color().setRGB(1, 0.5, 1),
-		new THREE.Color().setRGB(0.5, 0.5, 1),
-		new THREE.Color().setRGB(0.5, 0.5, 0.5)
-	];
+			new THREE.Color().setRGB(1, 0, 0),
+			new THREE.Color().setRGB(0, 1, 0),
+			new THREE.Color().setRGB(0, 0, 1),
+			new THREE.Color().setRGB(1, 1, 0),
+			new THREE.Color().setRGB(1, 0, 1),
+			new THREE.Color().setRGB(0, 1, 1),
+			new THREE.Color().setRGB(1, 0.5, 0.5),
+			new THREE.Color().setRGB(1, 0.5, 1),
+			new THREE.Color().setRGB(0.5, 0.5, 1),
+			new THREE.Color().setRGB(0.5, 0.5, 0.5)
+		],
+		voxelPosition = new THREE.Vector3();
 	
 	var setCubeType = function (cubeType) {
 		document.querySelector('.boxcolor').style.backgroundColor = '#' + cubeColors[cubeType].getHexString();
@@ -80,16 +81,78 @@ document.addEventListener('DOMContentLoaded', function() {
 	plane.rotation.x = -(Math.PI * 90 / 180);
 	scene.add(plane);
 	
-	var mouse2D = new THREE.Vector3(0, 10000, 0.5),
-		ray = new THREE.Ray(camera.position, null),
-		ambientLight = new THREE.AmbientLight(0x606060);
+	//var mouse2D = new THREE.Vector3(0, 10000, 0.5),
+		//ray = new THREE.Ray(camera.position, null),
+	var mouse2D = new THREE.Vector2(),
+		raycaster = new THREE.Raycaster(),
+		ambientLight = new THREE.AmbientLight(0x606060),
+		directionalLight = new THREE.DirectionalLight(0xffffff);
+	scene.add(ambientLight);
+	directionalLight.position.set(1, 1, 1).normalize();
+	scene.add(directionalLight);
+	
+	var stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '5px';
+	stats.domElement.style.right = '5px';
+	container.appendChild(stats.domElement);
+
+	var onDocumentMouseMove = function (e) {
+		mouse2D.x = (e.clientX / window.innerWidth) * 2 - 1;
+		mouse2D.y = - (e.clientY / window.innerHeight) * 2 + 1;
+	};
+	
+	document.addEventListener('mousemove', onDocumentMouseMove, false);
+	
+	var getRealIntersector = function (intersects) {
+		var intersector;
+		for (var i = 0; i < intersects.length; i++) {
+			intersector = intersects[i];
+			if (intersector.object != rollOverMesh) {
+				return intersector;
+			}
+		}
+	};
+	
+	var setVoxelPosition = function (intersector) {
+		var normalMatrix = new THREE.Matrix3();
+		normalMatrix.getNormalMatrix(intersector.object.matrixWorld);
+		var rotatedNormal = new THREE.Vector3().copy(intersector.face.normal);
+		rotatedNormal.applyMatrix3(normalMatrix);
+		//var tmpVec = new THREE.Vector3().copy(intersector.face.normal),
+		//voxelPosition.add(intersector.point, intersector.object.matrixRotationWorld.multiplyVector3(tmpVec));
+		voxelPosition.addVectors(intersector.point, rotatedNormal);
+		voxelPosition.x = Math.floor(voxelPosition.x / 50) * 50 + 25;
+		voxelPosition.y = Math.floor(voxelPosition.y / 50) * 50 + 25;
+		voxelPosition.z = Math.floor(voxelPosition.z / 50) * 50 + 25;
+	};
 	
 	var animate = function () {
 		requestAnimationFrame(animate);
 		render();
+		stats.update();
 	};
-	
+
 	var render = function () {
+		//var mouse3D = projector.unprojectVector(mouse2D.clone(), camera);
+		//var mouse3D = mouse2D.clone();
+		//mouse3D.unproject(camera);
+		//ray.direction = mouse3D.subSelf(camera.position).normalize();
+		//ray.direction = mouse3D.sub(camera.position).normalize();
+		//var intersects = ray.intersectScene(scene);
+		raycaster.setFromCamera(mouse2D, camera);
+		var intersects = raycaster.intersectObjects(scene.children);
+
+		if (intersects.length > 0) {
+			var intersector = getRealIntersector(intersects);
+
+			if (intersector) {
+				setVoxelPosition(intersector);
+				//rollOverMesh.position = voxelPosition;
+				rollOverMesh.position.copy(voxelPosition);
+			}
+		}
+		
 		renderer.render(scene, camera);
 	};
 	
