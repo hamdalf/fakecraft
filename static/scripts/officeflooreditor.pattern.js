ef89fa32c989f8380304299bcb34268f02af8663
Kyoung Woo Ham <kyoungwoo.ham@gmail.com>
2015-11-13 18:07:31 +0100

    office editor start

---
+++ static/scripts/officeflooreditor.pattern.js
@@ -0,0 +1,18 @@
+var cubePattern = {
+	floor: {
+		0: {
+			shading: THREE.FlatShading,
+			map: THREE.ImageUtils.loadTexture('/images/officeeditor/floor_wood.jpg')
+		},
+		1: {
+			shading: THREE.FlatShading,
+			color: new THREE.Color().setRGB(0.5, 0.5, 0.5)
+		}
+	},
+	wall: {
+		0: {
+			shading: THREE.FlatShading,
+			color: new THREE.Color().setRGB(1, 1, 0.96)
+		}
+	}
+};
