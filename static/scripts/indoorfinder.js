90a84234794ced32fe26e8ca513a7cbf282c04a1
hamdalf <kyoungwoo.ham@gmail.com>
2016-10-01 10:12:09 +0200

    more 2

--- static/scripts/indoorfinder.js
+++ static/scripts/indoorfinder.js
@@ -1195,7 +1195,7 @@ document.addEventListener('DOMContentLoaded', function() {
         //tmpCameraClone.translateX(window.innerWidth / 11);
         //tmpCameraClone.translateY(-(window.innerHeight / 12));
         tmpCameraClone.translateX(120);
-        tmpCameraClone.translateY(-30);
+        tmpCameraClone.translateY(-10);
         this.root.position.copy(tmpCameraClone.position);
         this.root.updateMatrix();
     };
